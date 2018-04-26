7698f3c22b8ca7877056832aa3767fd0117abb7e
Gianluca Campanella <gianluca@campanella.org>
2018-04-26 17:46:07 +0100

    Added theme_minimal() to convex function plot

--- slides/figures/convex_fun.R
+++ slides/figures/convex_fun.R
@@ -6,6 +6,7 @@ p <- tibble(
      ggplot(mapping = aes(x = x)) +
         geom_line(mapping = aes(y = x**2)) +
         geom_line(mapping = aes(y = x + 2), color = "red") +
+        theme_minimal() +
         xlab("x") + ylab("y")
 
 ggsave('convex_fun.pdf', p)
